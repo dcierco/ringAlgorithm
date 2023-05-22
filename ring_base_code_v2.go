3225035e5f0abf54dd2c853019b02eabcd1f3622
Daniel Cierco <daniel_cierco@live.com>
2023-05-22 20:57:10 +0000

    Adding basecode and chat-gpt one

--- ring_base_code_v2.go
+++ ring_base_code_v2.go
@@ -1,27 +1,28 @@
-// Código exemplo para o trabaho de sistemas distribuidos (eleicao em anel)
-// By Cesar De Rose - 2022
-
 package main
 
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 type mensagem struct {
-	tipo  int    // tipo da mensagem para fazer o controle do que fazer (eleição, confirmacao da eleicao)
-	corpo [3]int // conteudo da mensagem para colocar os ids (usar um tamanho ocmpativel com o numero de processos no anel)
+	tipo  int    // tipo da mensagem para fazer o controle do que fazer (eleição, confirmação da eleicao, novo coordenador)
+	corpo [3]int // conteudo da mensagem para colocar os ids (usar um tamanho compatível com o número de processos no anel)
 }
 
 var (
-	chans = []chan mensagem{ // vetor de canias para formar o anel de eleicao - chan[0], chan[1] and chan[2] ...
+	chans = []chan mensagem{ // vetor de canais para formar o anel de eleição - chan[0], chan[1], chan[2], ...
 		make(chan mensagem),
 		make(chan mensagem),
 		make(chan mensagem),
 		make(chan mensagem),
 	}
-	controle = make(chan int)
-	wg       sync.WaitGroup // wg is used to wait for the program to finish
+	controle       = make(chan int)
+	wg             sync.WaitGroup // wg is used to wait for the program to finish
+	numProcesses   = 4            // number of processes in the ring
+	coordinatorID  = 0            // initial coordinator ID
+	coordinatorSet = false        // flag to indicate if the coordinator is set
 )
 
 func ElectionControler(in chan int) {
@@ -29,25 +30,19 @@ func ElectionControler(in chan int) {
 
 	var temp mensagem
 
-	// comandos para o anel iciam aqui
-
-	// mudar o processo 0 - canal de entrada 3 - para falho (defini mensagem tipo 2 pra isto)
-
+	// Mudar o processo 0 - canal de entrada 3 - para falho (defini mensagem tipo 2 pra isso)
 	temp.tipo = 2
 	chans[3] <- temp
 	fmt.Printf("Controle: mudar o processo 0 para falho\n")
-
 	fmt.Printf("Controle: confirmação %d\n", <-in) // receber e imprimir confirmação
 
-	// mudar o processo 1 - canal de entrada 0 - para falho (defini mensagem tipo 2 pra isto)
-
+	// Mudar o processo 1 - canal de entrada 0 - para falho (defini mensagem tipo 2 pra isso)
 	temp.tipo = 2
 	chans[0] <- temp
 	fmt.Printf("Controle: mudar o processo 1 para falho\n")
 	fmt.Printf("Controle: confirmação %d\n", <-in) // receber e imprimir confirmação
 
-	// matar os outrs processos com mensagens não conhecidas (só pra cosumir a leitura)
-
+	// Matar os outros processos com mensagens não conhecidas (só para consumir a leitura)
 	temp.tipo = 4
 	chans[1] <- temp
 	chans[2] <- temp
@@ -58,14 +53,12 @@ func ElectionControler(in chan int) {
 func ElectionStage(TaskId int, in chan mensagem, out chan mensagem, leader int) {
 	defer wg.Done()
 
-	// variaveis locais que indicam se este processo é o lider e se esta ativo
-
 	var actualLeader int
-	var bFailed bool = false // todos inciam sem falha
+	var bFailed bool = false
 
-	actualLeader = leader // indicação do lider veio por parâmatro
+	actualLeader = leader
 
-	temp := <-in // ler mensagem
+	temp := <-in
 	fmt.Printf("%2d: recebi mensagem %d, [ %d, %d, %d ]\n", TaskId, temp.tipo, temp.corpo[0], temp.corpo[1], temp.corpo[2])
 
 	switch temp.tipo {
@@ -73,20 +66,35 @@ func ElectionStage(TaskId int, in chan mensagem, out chan mensagem, leader int) 
 		{
 			bFailed = true
 			fmt.Printf("%2d: falho %v \n", TaskId, bFailed)
-			fmt.Printf("%2d: lider atual %d\n", TaskId, actualLeader)
+			fmt.Printf("%2d: líder atual %d\n", TaskId, actualLeader)
 			controle <- -5
 		}
 	case 3:
 		{
 			bFailed = false
 			fmt.Printf("%2d: falho %v \n", TaskId, bFailed)
-			fmt.Printf("%2d: lider atual %d\n", TaskId, actualLeader)
+			fmt.Printf("%2d: líder atual %d\n", TaskId, actualLeader)
 			controle <- -5
+		}
+	case 1:
+		{
+			if bFailed {
+				// Process is failed, ignore election message
+				fmt.Printf("%2d: processo falho, ignorando mensagem de eleição\n", TaskId)
+			} else {
+				// Process is not failed, participate in the election
+				if temp.corpo[0] > actualLeader {
+					actualLeader = temp.corpo[0]
+					fmt.Printf("%2d: eleição em andamento, atualizando líder para %d\n", TaskId, actualLeader)
+				}
+				temp.corpo[TaskId] = TaskId
+				out <- temp // Pass the updated election message to the next process in the ring
+			}
 		}
 	default:
 		{
 			fmt.Printf("%2d: não conheço este tipo de mensagem\n", TaskId)
-			fmt.Printf("%2d: lider atual %d\n", TaskId, actualLeader)
+			fmt.Printf("%2d: líder atual %d\n", TaskId, actualLeader)
 		}
 	}
 
@@ -94,23 +102,40 @@ func ElectionStage(TaskId int, in chan mensagem, out chan mensagem, leader int) 
 }
 
 func main() {
+	wg.Add(numProcesses + 1) // Add a count for each process and the controller
 
-	wg.Add(5) // Add a count of four, one for each goroutine
-
-	// criar os processo do anel de eleicao
-
-	go ElectionStage(0, chans[3], chans[0], 0) // este é o lider
-	go ElectionStage(1, chans[0], chans[1], 0) // não é lider, é o processo 0
-	go ElectionStage(2, chans[1], chans[2], 0) // não é lider, é o processo 0
-	go ElectionStage(3, chans[2], chans[3], 0) // não é lider, é o processo 0
+	// Criar os processos do anel de eleição
+	for i := 0; i < numProcesses; i++ {
+		go ElectionStage(i, chans[i], chans[(i+1)%numProcesses], coordinatorID)
+	}
 
 	fmt.Println("\n   Anel de processos criado")
 
-	// criar o processo controlador
-
+	// Criar o processo controlador
 	go ElectionControler(controle)
 
 	fmt.Println("\n   Processo controlador criado\n")
 
-	wg.Wait() // Wait for the goroutines to finish\
+	// Simulação de solicitações externas
+	go func() {
+		for {
+			// Simular uma solicitação externa
+			time.Sleep(5 * time.Second)
+			fmt.Println("\nSolicitação externa recebida")
+
+			// Verificar se o coordenador está definido
+			if !coordinatorSet {
+				// Iniciar a eleição
+				coordinatorSet = true
+				fmt.Println("Iniciando eleição")
+				temp := mensagem{
+					tipo:  1,
+					corpo: [3]int{coordinatorID, coordinatorID, coordinatorID},
+				}
+				chans[0] <- temp
+			}
+		}
+	}()
+
+	wg.Wait() // Wait for the goroutines to finish
 }
