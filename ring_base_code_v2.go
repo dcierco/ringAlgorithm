3926815f5558a61ef5ae6e1db9c1fc1271b52c2c
Daniel Cierco <daniel_cierco@live.com>
2023-06-05 20:04:58 +0000

    returning to base code.

--- ring_base_code_v2.go
+++ ring_base_code_v2.go
@@ -1,3 +1,6 @@
+// Código exemplo para o trabaho de sistemas distribuidos (eleicao em anel)
+// By Cesar De Rose - 2022
+
 package main
 
 import (
@@ -6,102 +9,108 @@ import (
 )
 
 type mensagem struct {
-	tipo  int
-	corpo []int
+	tipo  int    // tipo da mensagem para fazer o controle do que fazer (eleição, confirmacao da eleicao)
+	corpo [3]int // conteudo da mensagem para colocar os ids (usar um tamanho ocmpativel com o numero de processos no anel)
 }
 
-var chans []chan mensagem
-var coordenadorID int
-var controle chan int
-var wg sync.WaitGroup
-
-func main() {
-	fmt.Println("\n   Anel de processos criado\n")
-
-	// Criar canais de comunicação
-	chans = make([]chan mensagem, 4)
-	for i := 0; i < 4; i++ {
-		chans[i] = make(chan mensagem)
+var (
+	chans = []chan mensagem{ // vetor de canias para formar o anel de eleicao - chan[0], chan[1] and chan[2] ...
+		make(chan mensagem),
+		make(chan mensagem),
+		make(chan mensagem),
+		make(chan mensagem),
 	}
-
 	controle = make(chan int)
+	wg       sync.WaitGroup // wg is used to wait for the program to finish
+)
 
-	// Criar goroutines para os processos
-	wg.Add(4)
-	go ElectionStage(0, chans[3], chans[0], controle)
-	go ElectionStage(1, chans[0], chans[1], controle)
-	go ElectionStage(2, chans[1], chans[2], controle)
-	go ElectionStage(3, chans[2], chans[3], controle)
-
-	// Processo controlador
-	fmt.Println("\n   Processo controlador criado\n")
-
-	// Mudar o processo 0 para falho
-	fmt.Println("Controle: mudar o processo 0 para falho")
-	chans[0] <- mensagem{tipo: 2, corpo: []int{}}
-	<-controle
-
-	// Mudar o processo 1 para falho
-	fmt.Println("Controle: mudar o processo 1 para falho")
-	chans[1] <- mensagem{tipo: 2, corpo: []int{}}
-	<-controle
-
-	fmt.Println("\n   Processo controlador concluído\n")
-
-	close(chans[3])
-	close(chans[2])
-	close(chans[1])
-	close(chans[0])
-	wg.Wait()
-}
-
-func ElectionStage(TaskId int, in chan mensagem, out chan mensagem, controle chan int) {
+func ElectionControler(in chan int) {
 	defer wg.Done()
 
-	var bFailed bool = false // Todos iniciam sem falha
+	var temp mensagem
 
-	for {
-		select {
-		case msg, ok := <-in:
-			if !ok {
-				// Canal fechado, terminar a goroutine
-				fmt.Printf("%2d: terminei \n", TaskId)
-				return
-			}
+	// comandos para o anel iciam aqui
 
-			fmt.Printf("%2d: recebi mensagem %d, [ %v ]\n", TaskId, msg.tipo, msg.corpo)
+	// mudar o processo 0 - canal de entrada 3 - para falho (defini mensagem tipo 2 pra isto)
 
-			switch msg.tipo {
-			case 2:
-				bFailed = true
-				fmt.Printf("%2d: falho %v \n", TaskId, bFailed)
-				fmt.Printf("%2d: coordenador atual %d\n", TaskId, coordenadorID)
-				controle <- -5
-			case 3:
-				bFailed = false
-				fmt.Printf("%2d: falho %v \n", TaskId, bFailed)
-				fmt.Printf("%2d: coordenador atual %d\n", TaskId, coordenadorID)
-				controle <- -5
-			case 1:
-				if !bFailed {
-					msg.corpo = append(msg.corpo, TaskId) // Inclui o ID do processo na mensagem
+	temp.tipo = 2
+	chans[3] <- temp
+	fmt.Printf("Controle: mudar o processo 0 para falho\n")
 
-					if len(msg.corpo) == len(chans) { // Volta completa, processo iniciador recebe a mensagem
-						if TaskId == msg.corpo[0] {
-							coordenadorID = TaskId
-							fmt.Printf("%2d: sou o coordenador\n", TaskId)
-						} else {
-							out <- msg // Repassa a mensagem
-						}
-					} else {
-						out <- msg // Repassa a mensagem
-					}
-				}
-			case 4:
-				fmt.Printf("%2d: não conheço este tipo de mensagem\n", TaskId)
-				fmt.Printf("%2d: coordenador atual %d\n", TaskId, coordenadorID)
-			}
+	fmt.Printf("Controle: confirmação %d\n", <-in) // receber e imprimir confirmação
 
+	// mudar o processo 1 - canal de entrada 0 - para falho (defini mensagem tipo 2 pra isto)
+
+	temp.tipo = 2
+	chans[0] <- temp
+	fmt.Printf("Controle: mudar o processo 1 para falho\n")
+	fmt.Printf("Controle: confirmação %d\n", <-in) // receber e imprimir confirmação
+
+	// matar os outrs processos com mensagens não conhecidas (só pra cosumir a leitura)
+
+	temp.tipo = 4
+	chans[1] <- temp
+	chans[2] <- temp
+
+	fmt.Println("\n   Processo controlador concluído\n")
+}
+
+func ElectionStage(TaskId int, in chan mensagem, out chan mensagem, leader int) {
+	defer wg.Done()
+
+	// variaveis locais que indicam se este processo é o lider e se esta ativo
+
+	var actualLeader int
+	var bFailed bool = false // todos inciam sem falha
+
+	actualLeader = leader // indicação do lider veio por parâmatro
+
+	temp := <-in // ler mensagem
+	fmt.Printf("%2d: recebi mensagem %d, [ %d, %d, %d ]\n", TaskId, temp.tipo, temp.corpo[0], temp.corpo[1], temp.corpo[2])
+
+	switch temp.tipo {
+	case 2:
+		{
+			bFailed = true
+			fmt.Printf("%2d: falho %v \n", TaskId, bFailed)
+			fmt.Printf("%2d: lider atual %d\n", TaskId, actualLeader)
+			controle <- -5
+		}
+	case 3:
+		{
+			bFailed = false
+			fmt.Printf("%2d: falho %v \n", TaskId, bFailed)
+			fmt.Printf("%2d: lider atual %d\n", TaskId, actualLeader)
+			controle <- -5
+		}
+	default:
+		{
+			fmt.Printf("%2d: não conheço este tipo de mensagem\n", TaskId)
+			fmt.Printf("%2d: lider atual %d\n", TaskId, actualLeader)
 		}
 	}
+
+	fmt.Printf("%2d: terminei \n", TaskId)
+}
+
+func main() {
+
+	wg.Add(5) // Add a count of four, one for each goroutine
+
+	// criar os processo do anel de eleicao
+
+	go ElectionStage(0, chans[3], chans[0], 0) // este é o lider
+	go ElectionStage(1, chans[0], chans[1], 0) // não é lider, é o processo 0
+	go ElectionStage(2, chans[1], chans[2], 0) // não é lider, é o processo 0
+	go ElectionStage(3, chans[2], chans[3], 0) // não é lider, é o processo 0
+
+	fmt.Println("\n   Anel de processos criado")
+
+	// criar o processo controlador
+
+	go ElectionControler(controle)
+
+	fmt.Println("\n   Processo controlador criado\n")
+
+	wg.Wait() // Wait for the goroutines to finish\
 }
